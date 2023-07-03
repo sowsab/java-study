925e1e8bcdd826a07ea68f2987cbba8b6b9ab3bb
sowsab <sowsab@naver.com>
2023-07-03 15:01:22 +0900

    study

--- ch06/S03.java
+++ ch06/S03.java
@@ -1,77 +1,77 @@
-package ch06;
+// package ch06;
 
-    class Cat {
-        String name;
-        int hp;
-        int position;
-        public Cat(String name, int hp, int position) {
-            this.name = name;
-            this.hp = hp;
-            this.position = position;
-        }
+//     class Cat {
+//         String name;
+//         int hp;
+//         int position;
+//         public Cat(String name, int hp, int position) {
+//             this.name = name;
+//             this.hp = hp;
+//             this.position = position;
+//         }
 
-        // 이름만 받고 피 100 포지션 0
+//         // 이름만 받고 피 100 포지션 0
 
-        public Cat (String name) {
-            this(name, 100, 0);
+//         public Cat (String name) {
+//             this(name, 100, 0);
             
-        }
+//         }
         
-        public void moveRight() {
-            this.position++;
-            System.out.println(this.name + "는 오른쪽으로 이동했습니다");
-            reduceHP();
-        }
+//         public void moveRight() {
+//             this.position++;
+//             System.out.println(this.name + "는 오른쪽으로 이동했습니다");
+//             reduceHP();
+//         }
 
-        public void moveLeft() {
-            if(this.position < 1) {
-                System.out.println(this.name + "는 왼쪽으로 갈 수 없습니다");
-                // 함수(메소드)에서 return을 만나면 아래 코드는 실행되지 않는다
-                return;
-            }
-            this.position--;
-            System.out.println(this.name + "는 왼쪽으로 이동했습니다");
-            reduceHP();
+//         public void moveLeft() {
+//             if(this.position < 1) {
+//                 System.out.println(this.name + "는 왼쪽으로 갈 수 없습니다");
+//                 // 함수(메소드)에서 return을 만나면 아래 코드는 실행되지 않는다
+//                 return;
+//             }
+//             this.position--;
+//             System.out.println(this.name + "는 왼쪽으로 이동했습니다");
+//             reduceHP();
             
-        }
+//         }
 
-        public void reduceHP() {
-            this.hp--;
-            System.out.println(this.name + " 체력이 내려갔습니다");
-        }
+//         public void reduceHP() {
+//             this.hp--;
+//             System.out.println(this.name + " 체력이 내려갔습니다");
+//         }
 
-        public void yam(int food) {
-            // 밥을 먹었을 때 체력이 100을 초과하는 경우
-            // 100으로 세팅하고 체력이 가득 찼다고 메시지 올리기
-            if (this.hp + food > 100) {
-                this.hp = 100;
-                System.out.println(this.name + "의 체력이 가득 찼습니다");
-                return;
-            }
-            this.hp = this.hp + food;
-            System.out.println(this.name + "은 밥을 먹었습니다");
-        }
+//         public void yam(int food) {
+//             // 밥을 먹었을 때 체력이 100을 초과하는 경우
+//             // 100으로 세팅하고 체력이 가득 찼다고 메시지 올리기
+//             if (this.hp + food > 100) {
+//                 this.hp = 100;
+//                 System.out.println(this.name + "의 체력이 가득 찼습니다");
+//                 return;
+//             }
+//             this.hp = this.hp + food;
+//             System.out.println(this.name + "은 밥을 먹었습니다");
+//         }
 
-        public String dong() {
-            return "이쁜 똥";
-        }
+//         public String dong() {
+//             return "이쁜 똥";
+//         }
 
-    }
+//     }
 
-public class S03 {
-    public static void main(String[] args) {
+// public class S03 {
+//     public static void main(String[] args) {
 
-        Cat cat = new Cat("야옹이");
+//         Cat cat = new Cat("야옹이");
 
-        cat.moveLeft();
+//         cat.moveLeft();
 
-        cat.moveRight();
+//         cat.moveRight();
         
-        cat.yam(5);
+//         cat.yam(5);
 
-        System.out.println(cat.hp);
+//         System.out.println(cat.hp);
 
-        System.out.println(cat.dong());
+//         System.out.println(cat.dong());
 
-    }
-}
+//     }
+// }
---
+++ ch06/S04.java
@@ -0,0 +1,11 @@
+package ch06;
+
+
+
+public class S04 {
+    public static void main(String[] args) {
+    
+        
+        
+    }
+}
