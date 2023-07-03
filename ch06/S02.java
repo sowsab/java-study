90c2c9bee17d323c55993ec9bb56f9c6acbee1dd
sowsab <sowsab@naver.com>
2023-07-03 13:43:23 +0900

    study

---
+++ ch06/S02.java
@@ -0,0 +1,19 @@
+package ch06;
+
+class Cat {
+    String name;
+    int age;
+    
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+}
+
+public class S02 {
+    public static void main(String[] args) {
+        
+    }
+}
