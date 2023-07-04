97a1cf06a122ae4451d08a812ea91049f9042c81
sowsab <sowsab@naver.com>
2023-07-04 10:31:16 +0900

    study

---
+++ ch06/S09.java
@@ -0,0 +1,20 @@
+// public class S09 {
+    
+//     class Cat{
+//         String name;
+
+//         public Cat(String name) {
+//             this.name = name;
+//         }
+
+//         public Cat() {
+//             String name = "길고양이";
+//             // this(name); // 에러, this()는 첫째줄에 적어야 한다
+//         }
+//     }
+
+//     public static void main(String[] args) {
+        
+//     }
+
+// }
