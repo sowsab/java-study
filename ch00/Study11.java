cde2bddf2b3fe66af73e028b7ca4149b89fdd1cf
sowsab <sowsab@naver.com>
2023-07-06 17:25:21 +0900

    study

--- ch00/Study11.java
+++ ch00/Study11.java
@@ -1,33 +1,33 @@
-package ch00;
+// package ch00;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+// import lombok.Getter;
+// import lombok.Setter;
+// import lombok.ToString;
 
-// 데이터를 고정적으로 제한할 때 사용한다
-enum Gender {
-    MALE, FEMALE
-}
+// // 데이터를 고정적으로 제한할 때 사용한다
+// enum Gender {
+//     MALE, FEMALE
+// }
 
-@Getter
-@Setter
-@ToString
-class Person {
-    private String name;
-    private String gender;
+// @Getter
+// @Setter
+// @ToString
+// class Person {
+//     private String name;
+//     private String gender;
 
-    public Person(String name, Gender gender) {
-        this.name = name;
-        this.gender = gender.name();
-    }
-}
+//     public Person(String name, Gender gender) {
+//         this.name = name;
+//         this.gender = gender.name();
+//     }
+// }
 
-public class Study11 {
-    public static void main(String[] args) {
+// public class Study11 {
+//     public static void main(String[] args) {
         
-        Person person = new Person("홍길동", Gender.MALE);
+//         Person person = new Person("홍길동", Gender.MALE);
 
-        System.out.println(person);
+//         System.out.println(person);
 
-    }
-}
+//     }
+// }
