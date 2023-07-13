9955fecda7440a4060d7d11de492028f499848a3
sowsab <sowsab@naver.com>
2023-07-13 16:56:10 +0900

    study

--- ch13/S02.java
+++ ch13/S02.java
@@ -27,12 +27,16 @@ public class S02 {
             // 쿼리문 실행
             resultSet = preparedStatement.executeQuery();
             // 결과값 확인
+            // Emp속성의 배열리스트를 만든다
             List<Emp> list = new ArrayList<>();
+            // index값이 없을때 까지 반복
             while (resultSet.next()) {
+                // Emp 속성의 emp를 생산한다
                 Emp emp = new Emp(
                     resultSet.getInt("employee_id"),
                     resultSet.getString("first_name"),
                     resultSet.getInt("salary"));
+                // emp를 list에 넣는다
                 list.add(emp);
             }
             System.out.println(list);
