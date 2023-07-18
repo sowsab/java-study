287a9acf3c98cd520b317b6119fed036c68bccfd
sowsab <sowsab@naver.com>
2023-07-18 11:44:11 +0900

    study

---
+++ ch00/Study12.java
@@ -0,0 +1,36 @@
+package ch00;
+
+// 싱글톤
+
+class Sun {
+
+    // 생성자를 private로 만들어서 new를 막는다
+    private Sun() {};
+
+    private static Sun sun = null;
+
+    
+    public static Sun getSun() {
+        if(sun == null) {
+            // 객체를 하나만 생성
+            sun = new Sun();
+        }
+        return sun;
+    }
+
+}
+
+public class Study12 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(Sun.getSun());
+        System.out.println(Sun.getSun());
+        System.out.println(Sun.getSun());
+        System.out.println(Sun.getSun());
+        System.out.println(Sun.getSun());
+        System.out.println(Sun.getSun());
+
+    }
+
+}
